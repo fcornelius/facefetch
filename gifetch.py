8d85e342fa3752a243872f83491cad2324bc49c3
Felix Cornelius <mail@felixcornelius.de>
2016-10-09 03:21:47 +0200

    Switched to Bing Image search for easier parsing w/o api

--- gifetch.py
+++ gifetch.py
@@ -4,16 +4,17 @@
 import argparse
 import os
 import sys
-import urllib
+import urllib.request
 from bs4 import BeautifulSoup
 
 
 def main():
 
-    sizes = ['l', 'm', 'i', 'qsvga', 'vga', 'svga', 'xga', '2mp', '4mp', '8mp', '10mp']
-    types = ['face', 'photo', 'clip', 'line', 'anim']
+    sizes = ['all', 'small', 'medium', 'large', 'xlarge']
+    types = ['photo', 'clipart', 'lineart', 'anim']
+    face  = ['closeup', 'portrait']
 
-    p = argparse.ArgumentParser(description='Batch downloads a set amount of images in seperate subfolders matching the Google Image search query of each folder name.')
+    p = argparse.ArgumentParser(description='Batch downloads a set amount of images in seperate subfolders matching the Bing Image search query of each folder name.')
     p.add_argument('path',
                    default=os.getcwd(),
                    nargs='?',
@@ -24,17 +25,23 @@ def main():
                    help='number of images to download in each directory')
     p.add_argument('--size', '-s',
                    choices=sizes,
-                   default='m',
+                   default='medium',
                    nargs='?',
                    metavar='s',
-                   help='(optional) minimum image size. Values: ' + ', '.join(sizes) + '. defaults to m'
+                   help='(optional) minimum image size. Values: ' + ', '.join(sizes) + '. defaults to medium'
                    )
     p.add_argument('--type', '-t',
                    choices=types,
                    default='',
                    nargs='?',
                    metavar='t',
-                   help='(optional) image type. Values: ' + ', '.join(types) + ' (optional)')
+                   help='(optional) image type. Values: ' + ', '.join(types))
+    p.add_argument('--face', '-f',
+                   choices=face,
+                   default='',
+                   nargs='?',
+                   metavar='f',
+                   help='(optional) images with faces. Values: ' + ', '.join(face))
 
     args = p.parse_args()
     # print(args)
@@ -44,6 +51,8 @@ def main():
     if not dirs:
         sys.exit('No subfolders. Add one subfolder for each image query')
 
+    # Size query params
+
     fetcher = ImageFetcher(args)
     for query in dirs:
         urls = fetcher.collect_urls(query)
@@ -51,21 +60,52 @@ def main():
         # fetcher.store_images(urls,query)
 
 
+
+
+
 class ImageFetcher:
     def __init__(self, args):
-        self.args = args
+        self.args = self.set_query_params(args)
 
     def collect_urls(self, query):
         urls = []
-        query_string = 'https://www.google.de/search?q={}&tbm=isch&tbs=isz:{},itp:{}'
-        query_string = query_string.format(query.replace(' ','+'), self.args.size, self.args.type)
+        query_string = 'https://www.bing.com/images/search?&q={}&qft={}{}{}'
+        query_string = query_string.format(query.replace(' ','+'), self.args.size, self.args.type, self.args.face)
 
-        response = urllib.request.urlopen(query_string)
-        return urls
+        req = urllib.request.Request(query_string, headers={'User-Agent' : "gifetch"})
+        res = urllib.request.urlopen(req)
+        html = res.read()
+        soup = BeautifulSoup(html, 'lxml')
+        imgs = soup.select('a[class=thumb]')
+
+        for i in range(1, self.args.n):
+            url = img[i]['href']
+
+
 
     def store_images(self, urls, dir):
         pass
 
+    @staticmethod
+    def set_query_params(args):
+        if args.size == 'all':
+            args.size = 'wallpaper'
+        args.size = '+filterui:imagesize-' + args.size
+        if args.size == 'all': args.size = ''
+
+        if args.type == 'lineart':
+            args.type = 'linedrawing'
+        elif args.type == 'anim':
+            args.type = 'animatedgif'
+        if args.type:
+            args.type = '+filterui:photo-' + args.type
+
+        if args.face == 'closeup':
+            args.face = 'face'
+        if args.face:
+            args.face = '+filterui:face-' + args.face
+
+        return args
 
 
 if __name__ == '__main__':
